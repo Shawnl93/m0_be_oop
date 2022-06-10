fa81d7f0cd772daba1360ce0216a48a3d2593e38
Shawn Lee <Shawnl93@hotmail.com>
2022-06-10 17:58:52 -0600

    Initial commit

--- oop.rb
+++ oop.rb
@@ -2,6 +2,18 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn
+  def initialize(name)
+    @name = name
+    @color = "silver"
+  end
+
+  def say
+    "*~* hello my name is #{@name} *~*"
+  end
+end
+
+uni1 = Unicorn.new("Shawn")
 
 
 
@@ -11,6 +23,20 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+  def initialize(name, pet)
+    @name = name
+    @pet = pet
+    @thirsty = true
+  end
+
+  def drink
+    @thristy = false
+  end
+
+end
+
+vamp1 = Vampire.new("Shawn", "Bat")
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,6 +44,27 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon
+  def initialize(name, rider, color)
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = true
+  end
+
+  def eat(meals)
+    if meals >= 4
+      @is_hungry = false
+    elsif meals <= 3
+      @is_hungry = true
+    end
+  end
+end
+
+dragon1 = Dragon.new("Toothless", "Shawn", "Black")
+
+print dragon1.eat(1)
 
 
 #  Write a Hobbit class
@@ -28,3 +75,39 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+  def initialize(name, disposition)
+    @name = name
+    @disposition = disposition
+    @age = 0
+    @is_adult = false
+    @is_old = false
+    @has_ring = false
+    if @name == "Frodo"
+      @has_ring = true
+    end
+  end
+
+  def celebrate_birthday
+    @age += 51
+
+    if @age >= 33
+    @is_adult = true
+    end
+
+    if @age >= 101
+    @is_old = true
+    end
+
+    if @name == "Frodo"
+    @has_ring = true
+    end
+  end
+end
+
+hobbit1 = Hobbit.new("Frodo", "Funny")
+
+hobbit1.celebrate_birthday
+
+p hobbit1
